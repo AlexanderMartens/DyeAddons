1ddc3b96d68366838bd7dc0806b68cf8bfed2cf5
AlexanderMartens <amartens000@gmail.com>
2026-05-27 19:54:36 -0500

    Added companion object to DyeAddons

--- src/main/kotlin/anlg/dyeaddons/DyeAddons.kt
+++ src/main/kotlin/anlg/dyeaddons/DyeAddons.kt
@@ -1,14 +1,28 @@
 package anlg.dyeaddons
 
 import net.fabricmc.api.ClientModInitializer
+import net.minecraft.client.Minecraft
 import org.slf4j.LoggerFactory
 
-object DyeAddons : ClientModInitializer {
-	internal const val MOD_ID = "dyeaddons"
-    internal val logger = LoggerFactory.getLogger(MOD_ID)
+class DyeAddons : ClientModInitializer {
+	companion object {
+		internal const val MOD_ID = "dyeaddons"
+
+		internal val logger = LoggerFactory.getLogger(MOD_ID)
+
+		@JvmField
+		val mc: Minecraft = Minecraft.getInstance()
+
+		@JvmStatic
+		lateinit var INSTANCE: DyeAddons
+			private set
+	}
+
+	init {
+		INSTANCE = this
+	}
 
 	override fun onInitializeClient() {
-
 
 		logger.info("Dye Addons initialized!")
 	}
