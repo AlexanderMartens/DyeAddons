4d07a5e6d9db2f35af97f2e212ad79a077a75276
AlexanderMartens <amartens000@gmail.com>
2026-08-10 10:11:23 -0500

    Improved Puddle Jumper Timer

--- src/main/kotlin/anlg/dyeaddons/features/qol/PuddleJumperTimer.kt
+++ src/main/kotlin/anlg/dyeaddons/features/qol/PuddleJumperTimer.kt
@@ -8,6 +8,7 @@ import anlg.dyeaddons.events.models.ClientTickEvent
 import anlg.dyeaddons.events.models.EntityDespawnEvent
 import anlg.dyeaddons.events.models.EntitySpawnEvent
 import anlg.dyeaddons.events.models.MobKillEvent
+import anlg.dyeaddons.events.models.ServerTickEvent
 import anlg.dyeaddons.events.models.WorldChangedEvent
 import anlg.dyeaddons.gui.overlay.TextOverlayData
 import anlg.dyeaddons.gui.overlay.TextOverlayProvider
@@ -27,7 +28,8 @@ object PuddleJumperTimer : TextOverlayProvider {
     data class TrackedPuddleJumper(
         val frog: TrackedFrog,
         var jumpStage: Int = 0, // 0 is when it first spawns, increments when it starts/stops jumping, odd = jumping, even = not jumping, dies a few seconds after stage 8
-        var deathTimer: Int = 0
+        var deathTimer: Int = 0,
+        val tickStages: MutableList<Int> = mutableListOf()
     )
 
     private val PUDDLE_JUMPER_MESSAGE = Regex("""A Puddle Jumper is preparing for liftoff—cast your rod into it and hold on tight!""")
@@ -38,7 +40,6 @@ object PuddleJumperTimer : TextOverlayProvider {
 
     private var tickCounter = 0
     private const val TICK_WINDOW = 1
-    private const val PUDDLE_JUMPER_DEATH = 320 // Dies 16 seconds after jump stage 7
 
     override var textOverlayData = TextOverlayData()
     override val defaultWidth: Int = 150
@@ -53,6 +54,7 @@ object PuddleJumperTimer : TextOverlayProvider {
         EventBus.subscribe(EntitySpawnEvent::class, ::onEntitySpawn)
         EventBus.subscribe(EntityDespawnEvent::class, ::onEntityDespawn)
         EventBus.subscribe(ClientTickEvent::class, ::onTick)
+        EventBus.subscribe(ServerTickEvent::class, ::onServerTick)
         EventBus.subscribe(WorldChangedEvent::class, ::onWorldChange)
     }
 
@@ -74,7 +76,7 @@ object PuddleJumperTimer : TextOverlayProvider {
 
         frogs.remove(event.entity.id)
         if (puddleJumpers.containsKey(event.entity.id)) {
-            DyeAddons.debug("Puddle Jumper Died at ${puddleJumpers[event.entity.id]?.deathTimer} ticks", DebugCategories.KILL_EVENT)
+            DyeAddons.debug("Puddle Jumper Died at $tickCounter ticks, Predicted: ${puddleJumpers[event.entity.id]?.deathTimer}", DebugCategories.KILL_EVENT)
             EventBus.publish(MobKillEvent("Puddle Jumper", 0.0,null))
         }
         puddleJumpers.remove(event.entity.id)
@@ -82,8 +84,6 @@ object PuddleJumperTimer : TextOverlayProvider {
 
     private fun onTick(@Suppress("UNUSED_PARAMETER") event: ClientTickEvent) {
         if (!SkyblockUtils.isInSkyblock() || SkyblockUtils.getWorldName() != "Lotus Atoll") return
-
-        tickCounter++
 
         val frogsIterator = frogs.iterator()
 
@@ -97,6 +97,7 @@ object PuddleJumperTimer : TextOverlayProvider {
             if (pending.entity.scale == 4f && fishedPuddleJumper) {
                 frogsIterator.remove()
                 puddleJumpers[id] = TrackedPuddleJumper(pending)
+                puddleJumpers[id]?.tickStages?.add(tickCounter)
                 continue
             }
 
@@ -113,28 +114,52 @@ object PuddleJumperTimer : TextOverlayProvider {
 
             if ((puddleJumper.jumpStage % 2 == 1) xor puddleJumper.frog.entity.jumpAnimationState.isStarted) {
                 puddleJumper.jumpStage++
+                puddleJumper.tickStages.add(tickCounter)
             }
 
-            if (puddleJumper.jumpStage >= 7) puddleJumper.deathTimer++
+            if (puddleJumper.jumpStage >= 8 && puddleJumper.deathTimer == 0) puddleJumper.deathTimer = predictDeathTick(puddleJumper.tickStages)
 
+            if (puddleJumper.deathTimer != 0 && puddleJumper.deathTimer < tickCounter - 40) puddleJumper.deathTimer = tickCounter + 80 // Incorrectly thought puddle jumper dies in 4s
         }
 
         textOverlayData.lines = puddleJumpers.values.map {
             Component.literal("${RED}Puddle Jumper${WHITE}: $YELLOW" +
-                    when(it.deathTimer) {
-                        in 0..(PUDDLE_JUMPER_DEATH - 200) -> "Jumping"
-                        in (PUDDLE_JUMPER_DEATH - 200)..(PUDDLE_JUMPER_DEATH - 100) -> "Dying in ${YELLOW}${"%.2f".format((PUDDLE_JUMPER_DEATH - it.deathTimer) / 20f)}s"
-                        in (PUDDLE_JUMPER_DEATH - 100)..(PUDDLE_JUMPER_DEATH - 40) -> "Dying in ${GOLD}${"%.2f".format((PUDDLE_JUMPER_DEATH - it.deathTimer) / 20f)}s"
-                        in (PUDDLE_JUMPER_DEATH - 40)..PUDDLE_JUMPER_DEATH -> "Dying in ${RED}${"%.2f".format((PUDDLE_JUMPER_DEATH - it.deathTimer) / 20f)}s"
-                        else -> "Dying in ${RED}Soon"
+                    if (it.deathTimer == 0) {
+                        "Jumping"
+                    } else {
+                        when(it.deathTimer - tickCounter) {
+                            in 100..200 -> "Dying in ${YELLOW}${"%.2f".format((it.deathTimer - tickCounter) / 20f)}s"
+                            in 40..100 -> "Dying in ${GOLD}${"%.2f".format((it.deathTimer - tickCounter) / 20f)}s"
+                            in 0..40 -> "Dying in ${RED}${"%.2f".format((it.deathTimer - tickCounter) / 20f)}s"
+                            else -> "Dying in ${RED}Soon"
+                        }
                     } +
                     "${YELLOW}!")
         }
+    }
 
+    private fun onServerTick(event: ServerTickEvent) {
+        if (!SkyblockUtils.isInSkyblock() || SkyblockUtils.getWorldName() != "Lotus Atoll") return
+
+        tickCounter = event.tick
     }
 
     private fun onWorldChange(@Suppress("UNUSED_PARAMETER") event: WorldChangedEvent) {
         frogs.clear()
         puddleJumpers.clear()
     }
+
+    private fun predictDeathTick(stages: List<Int>): Int {
+        if (stages.size < 9) return tickCounter + 200
+
+        val stage7 = stages[7]
+        val stage8 = stages[8]
+
+        val interval78 = stage8 - stage7
+
+        val predictedDeath = stage8 + if (interval78 < 120) 80 else 200
+
+        return predictedDeath
+    }
+
 }
